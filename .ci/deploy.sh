ae9f5450b8052929967f548445f17da81b528158
TheMrMilchmann <themrmilchmann@gmail.com>
2017-09-08 19:19:46 +0200

    build: fix script path
    Signed-off-by: TheMrMilchmann <themrmilchmann@gmail.com>

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -10,7 +10,6 @@ if [ "$TRAVIS_REPO_SLUG" == "$SLUG" ] && [ "$TRAVIS_JDK_VERSION" == "$JDK" ] && 
 
     # Upload snapshot artifacts to OSSRH.
 
-    cd ../
     source ./gradlew.sh uploadArchives --parallel -Psnapshot
 
     # Upload latest Javadoc to Github pages.
