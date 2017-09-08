eeae36588c319da046fa2eed2d1881e3003403a2
TheMrMilchmann <themrmilchmann@gmail.com>
2017-09-08 21:06:13 +0200

    build: fix work dir
    hopefully ...
    
    Signed-off-by: TheMrMilchmann <themrmilchmann@gmail.com>

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -10,6 +10,7 @@ if [ "$TRAVIS_REPO_SLUG" == "$SLUG" ] && [ "$TRAVIS_JDK_VERSION" == "$JDK" ] && 
 
     # Upload snapshot artifacts to OSSRH.
 
+    cd $TRAVIS_BUILD_DIR
     source ./gradlew.sh uploadArchives --parallel -Psnapshot
 
     # Upload latest Javadoc to Github pages.
