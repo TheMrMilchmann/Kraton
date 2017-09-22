c02170ab22b0d0bb1d1e2b468f6a21c942ab0828
TheMrMilchmann <themrmilchmann@gmail.com>
2017-09-22 13:46:45 +0200

    fix(lang-java): fix integration test results
    Signed-off-by: TheMrMilchmann <themrmilchmann@gmail.com>

--- modules/lang-java/src/test-integration/resources/java/com/github/themrmilchmann/kraton/test/lang/java/types/TAMBE.java
+++ modules/lang-java/src/test-integration/resources/java/com/github/themrmilchmann/kraton/test/lang/java/types/TAMBE.java
@@ -5,7 +5,6 @@ import com.github.themrmilchmann.kraton.test.lang.java.types.a.Map;
 class TAMBE {
 
     Map aMap;
-
     com.github.themrmilchmann.kraton.test.lang.java.types.b.Map.Entry bMapEntry;
 
 }
--- modules/lang-java/src/test-integration/resources/java/com/github/themrmilchmann/kraton/test/lang/java/types/TAMBM.java
+++ modules/lang-java/src/test-integration/resources/java/com/github/themrmilchmann/kraton/test/lang/java/types/TAMBM.java
@@ -5,7 +5,6 @@ import com.github.themrmilchmann.kraton.test.lang.java.types.a.Map;
 class TAMBM {
 
     Map aMap;
-
     com.github.themrmilchmann.kraton.test.lang.java.types.b.Map bMap;
 
 }
